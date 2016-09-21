d916db9cface032f9f12c4419779967ab64f98b5
张泉 <837560726@qq.com>
2016-09-21 16:15:08 +0800

    Initial Commit

---
+++ 图片游览器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  图片游览器
+//
+//  Created by 诠释 on 16/9/21.
+//  Copyright © 2016年 诠释. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
